3f80da26dcc42cc6a6df78cc31ba2bae928de003
DPK <k.dhaam@gmail.com>
2022-02-17 14:16:08 +0100

    initial commit with files

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Doodle - Home </title>
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+    <div class="wrapper indexPage">
+        <div class="mainSection">
+            <div class="logoContainer">
+                <img src="images/logo.jpg" alt="Doodle Logo">
+            </div>
+            <div class="searchContainer">
+                <form action="search.php" method="GET"> 
+                    <input class="searchBox" type="text" name="term" placeholder="Search...">
+                    <input class="searchButton" type="submit" value="Search">
+                </form>
+            </div>
+        </div>
+    </div> 
+</body>
+</html>
