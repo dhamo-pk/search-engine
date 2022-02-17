b8f9eba3e5bb43c6e5c909fdbcf7a4677fc3324b
DPK <k.dhaam@gmail.com>
2022-02-17 20:26:34 +0100

    crawler to extract all href in a web page

--- crawl.php
+++ crawl.php
@@ -1,6 +1,41 @@
 <?php
 include("classes/DomDocumentParser.php");
 
+function createLink($src, $url)
+{
+    echo "SRC: $src<br>";
+    // echo "URL: $url<br>";
+
+    $scheme = parse_url($url)["scheme"]; //http
+    $host = parse_url($url)["host"]; //www.name.com
+
+    #//www.name.com
+    if (substr($src, 0, 2) == "//") {
+        $src = $scheme . ":" . $src; # http://www.name.com
+    } 
+    # /page.php
+    else if (substr($src, 0, 1) == "/") {
+        $src = $scheme . "://" . $host . $src; # http://www.name.com/page.php
+    }
+
+    # ./page.php
+    else if (substr($src, 0, 2) == "./") {
+        $src = $scheme . "://" . $host . dirname(parse_url($url)["path"]) . substr($src,1); 
+    }
+   
+    #../path/page.php
+    else if (substr($src, 0, 3) == "../") {
+        $src = $scheme . "://" . $host . "/" . $src;
+    }
+
+    #www.name.com/page.php
+    else if (substr($src, 0, 4) == "https" || substr($src, 0, 3) == "http") {
+        $src = $scheme . "://" . $host . "/" .  $src;
+    }
+    echo "Final SRC: $src<br><br><br>";
+    return $src;
+}
+
 function followLinks($url)
 {
     //echo $url;
@@ -8,13 +43,21 @@ function followLinks($url)
 
     $linkList = $parser->getLinks();
 
-    foreach($linkList as $link) {
-        $href = $link -> getAttribute("href");
-        echo $href . "<br>"; 
+    foreach ($linkList as $link) {
+        $href = $link->getAttribute("href");
+        #echo $href . "<br>"; 
+
+        if (strpos($href, "#") !== false) {
+            continue;
+        } else if (substr($href, 0, 11) == "javascript:") {
+            continue;
+        }
+
+        $href = createLink($href, $url);
+
+        echo $href . "<br>";
     }
 }
 
-$startUrl = "https://www.stackoverflow.com/";
-    followLinks($startUrl);
-
-?>
+$startUrl = "https://www.bbc.com/";
+followLinks($startUrl);
